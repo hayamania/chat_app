74925c8c79743fb4a1407249de872a1aa1002fd5
Chie Hym <hayama.chie@gmail.com>
2024-01-22 18:45:23 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="p-8 bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="Shoes"
+    />
+  </figure>
+  <div class="mt-4">
+    <h2 class="text-2xl font-bold">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+  </div>
+  <div class="mt-4">
+    <button class="btn btn-primary">Buy Now</button>
+  </div>
+</div>
